539b417ee39ee53e70f9f66859554592857c7e76
unknown <niambaust17@gmail.com>
2021-02-14 11:56:17 +0600

    validation and some feature added

--- app.js
+++ app.js
@@ -12,26 +12,42 @@ let sliders = [];
 // Find the name in the url and go to their website
 // to create your own api key
 const KEY = '15674931-a9d714b6e9d654524df198e00&q';
-
+const toggleSpinner = () =>
+{
+  const spinner = document.getElementById('loading-spinner');
+  spinner.classList.toggle('d-none');
+}
 // show images 
 const showImages = (images) =>
 {
-  imagesArea.style.display = 'block';
-  gallery.innerHTML = '';
-  // show gallery title
-  galleryHeader.style.display = 'flex';
-  images.forEach(image =>
+  if (images.length !== 0)
   {
-    let div = document.createElement('div');
-    div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
-    div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${ image.webformatURL }") src="${ image.webformatURL }" alt="${ image.tags }">`;
-    gallery.appendChild(div)
-  })
+    imagesArea.style.display = 'block';
+    gallery.innerHTML = '';
+    // show gallery title
+    galleryHeader.style.display = 'flex';
+    toggleSpinner();
+    images.forEach(image =>
+    {
+      let div = document.createElement('div');
+      div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
+      div.innerHTML = ` <img id="image-status" class="img-fluid img-thumbnail" onclick=selectItem(event,"${ image.webformatURL }") src="${ image.webformatURL }" alt="${ image.tags }">`;
+      gallery.appendChild(div)
+    })
+  }
+  else
+  {
+    alert("No image found for this type. Try Again with a another type.");
+    toggleSpinner();
+    imagesArea.style.display = 'none';
+  }
+
 
 }
 
 const getImages = (query) =>
 {
+  toggleSpinner();
   fetch(`https://pixabay.com/api/?key=${ KEY }=${ query }&image_type=photo&pretty=true`)
     .then(response => response.json())
     .then(data => showImages(data.hits))
@@ -42,17 +58,25 @@ let slideIndex = 0;
 const selectItem = (event, img) =>
 {
   let element = event.target;
-  element.classList.add('added');
-
+  toggleImage(element);
   let item = sliders.indexOf(img);
   if (item === -1)
   {
     sliders.push(img);
-  } else
+  }
+  else
   {
-    alert('Hey, Already added !')
+    sliders.splice(item, 1);
   }
 }
+
+// toggle select or unselect image
+const toggleImage = (element) =>
+{
+  element.classList.toggle('added');
+}
+
+
 var timer
 const createSlider = () =>
 {
@@ -76,30 +100,23 @@ const createSlider = () =>
   document.querySelector('.main').style.display = 'block';
   // hide image aria
   imagesArea.style.display = 'none';
-  const duration = document.getElementById('duration').value || 1000;
-  if (duration < 0)
+  const duration = Math.abs(document.getElementById('duration').value) || 1000;
+
+  sliders.forEach(slide =>
   {
-    alert("Duration can't be negative");
-    document.querySelector('.main').style.display = 'none';
-  }
-  else
-  {
-    sliders.forEach(slide =>
-    {
-      let item = document.createElement('div')
-      item.className = "slider-item";
-      item.innerHTML = `<img class="w-100"
+    let item = document.createElement('div')
+    item.className = "slider-item";
+    item.innerHTML = `<img class="w-100"
         src="${ slide }"
         alt="">`;
-      sliderContainer.appendChild(item)
-    })
-    changeSlide(0)
-    timer = setInterval(function ()
-    {
-      slideIndex++;
-      changeSlide(slideIndex);
-    }, duration);
-  }
+    sliderContainer.appendChild(item)
+  })
+  changeSlide(0)
+  timer = setInterval(function ()
+  {
+    slideIndex++;
+    changeSlide(slideIndex);
+  }, duration);
 
 }
 
@@ -139,8 +156,15 @@ searchBtn.addEventListener('click', function ()
   document.querySelector('.main').style.display = 'none';
   clearInterval(timer);
   const search = document.getElementById('search');
-  getImages(search.value)
-  sliders.length = 0;
+  if (isNaN(search.value))
+  {
+    getImages(search.value)
+    sliders.length = 0;
+  }
+  else
+  {
+    alert('Please enter which type of image you want to show');
+  }
 })
 
 sliderBtn.addEventListener('click', function ()
